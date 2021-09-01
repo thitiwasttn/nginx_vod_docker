c75122bc5d0b6c6b57148787275896105cb8783a
GA007007\Admin <thitiwas.n@g-able.com>
2021-09-01 11:20:31 +0700

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-docker build -t nginx_vod:0 .
+docker build -t nginx_vod:0.1 .
+
+docker stop nginx_vod
+
+docker rm nginx_vod
+
+docker run --name nginx_vod -d -it -p82:80 nginx_vod:0.1
+
+docker exec -it nginx_vod bash
