217ac386888fe5ae29fc39e533452a99c47be7cd
GA007007\Admin <thitiwas.n@g-able.com>
2021-09-01 15:26:46 +0700

    up

--- run.bat
+++ run.bat
@@ -4,6 +4,6 @@ docker stop nginx_vod
 
 docker rm nginx_vod
 
-docker run --name nginx_vod -d -it -p82:80 nginx_vod:0.1
+docker run --name nginx_vod -d -it -p82:80 -v vod_videos:/vod/videos nginx_vod:0.1
 
 docker exec -it nginx_vod bash
